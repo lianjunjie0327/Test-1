16394da83bd77235d1972a2de0f2590732a33240
fzj <fzj0314@163.com>
2018-01-05 16:48:05 +0800

    新建文件

---
+++ src/com/zhiyou100/git/Test02.java
@@ -0,0 +1,16 @@
+package com.zhiyou100.git;
+
+/**  
+* @ClassName: Test02  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:46:38
+*  
+*/
+public class Test02 {
+	public static void main(String[] args) {
+		System.out.println("新增文件");
+	}
+
+}
+
