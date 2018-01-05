b89d63efdd6c836de2826ea970ba0cfc359ad2b6
fzj <fzj0314@163.com>
2018-01-05 16:45:48 +0800

    新建个项目

---
+++ src/com/zhiyou100/git/Test.java
@@ -0,0 +1,17 @@
+package com.zhiyou100.git;
+
+/**  
+* @ClassName: Test  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:41:35
+*  
+*/
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("新建测试项目");
+	}
+
+}
+
